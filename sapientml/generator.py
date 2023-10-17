97ad226e0b3ea198768a6bc346712ed9a8f48c99
Kosaku Kimura <kimura.kosaku@fujitsu.com>
2023-10-17 09:28:00 +0900

    fix: modify function signatures of GeneratedModel (#55)
    * WIP
    
    Signed-off-by: Kosaku Kimura <kimura.kosaku@fujitsu.com>
    
    * fix: modify function signatures of GeneratedModel
    
    Signed-off-by: Kosaku Kimura <kimura.kosaku@fujitsu.com>
    
    * add tests
    
    Signed-off-by: Kosaku Kimura <kimura.kosaku@fujitsu.com>
    
    ---------
    
    Signed-off-by: Kosaku Kimura <kimura.kosaku@fujitsu.com>

--- sapientml/generator.py
+++ sapientml/generator.py
@@ -6,23 +6,23 @@ from .params import Code, Dataset, Task
 
 
 class CodeBlockGenerator(ABC):
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs):  # pragma: no cover
         pass
 
     @abstractmethod
-    def generate_code(self, dataset: Dataset, task: Task) -> Tuple[Dataset, Code]:
+    def generate_code(self, dataset: Dataset, task: Task) -> Tuple[Dataset, Code]:  # pragma: no cover
         pass
 
 
 class PipelineGenerator(ABC):
     @abstractmethod
-    def generate_pipeline(self, dataset: Dataset, task: Task):
+    def generate_pipeline(self, dataset: Dataset, task: Task):  # pragma: no cover
         pass
 
     @abstractmethod
-    def save(self, output_dir: Union[Path, str]):
+    def save(self, output_dir: Union[Path, str]):  # pragma: no cover
         pass
 
     @abstractmethod
-    def add_explanation(self):
+    def add_explanation(self):  # pragma: no cover
         pass
