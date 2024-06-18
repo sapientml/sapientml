3dee6a38ba117a6b1f7dcf2eaa091d51a90cf55d
Kosaku Kimura <kimura.kosaku@fujitsu.com>
2024-06-18 23:19:59 +0900

    fix: s/localfile/datastore/
    Signed-off-by: Kosaku Kimura <kimura.kosaku@fujitsu.com>

--- sapientml/main.py
+++ sapientml/main.py
@@ -339,7 +339,7 @@ class SapientML:
         self.params = {"model_type": self.model_type}
         self.params.update(self.task.model_dump())
         self.params.update(self.config.model_dump())
-        self.params.update(self.generator.loaddata.config.model_dump())
+        self.params.update(self.generator.datastore.config.model_dump())
         self.params.update(self.generator.preprocess.config.model_dump())
 
         self.model = GeneratedModel(
